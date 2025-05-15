5f20a1aae3631cc241fa87cb06bf8d41ad196736
Jonghak Bae <jhbae15@naver.com>
2025-05-15 19:23:58 +0900

    Modify mpc constraints

--- controller/mpc.py
+++ controller/mpc.py
@@ -5,7 +5,7 @@ import casadi as ca
 
 
 class MPC:
-    def __init__(self, vehicle, dt, ref_speed, ref_lane, init_state, horizon=10, d_safe=3):
+    def __init__(self, vehicle, dt, ref_speed, ref_lane, init_state, horizon=10, ds_safe=3, dn_safe = 0.1):
         self.vehicle = vehicle
         self.dt = dt
         self.ref_speed = ref_speed
@@ -13,13 +13,19 @@ class MPC:
         self.N = horizon
         self.state_n = 4
         self.control_n = 2
-        self.Length = 5
+        self.Length = vehicle.LENGTH
+        self.Width = vehicle.WIDTH
         self.init_state = init_state
+        
+        self.max_vel = 40.0
         self.max_a = 5.0
         self.max_steering_angle = radians(5)
-        self.d_safe = d_safe
+        self.ds_safe = ds_safe
+        self.dn_safe = dn_safe
 
-        self.max_obs_n = 5
+        self.max_difflaneobs_n = 3
+        self.max_samelaneobs_n = 3
+
 
         self.ocp = self._build_ocp()
         self.solver = AcadosOcpSolver(self.ocp, json_file='acados_ocp.json')
@@ -52,13 +58,18 @@ class MPC:
         vref = SX.sym('vref')
         psiref = SX.sym('psiref')
         ref = ca.vertcat(xref, yref, vref, psiref)
-        obstacles = []
-        for i in range(self.max_obs_n):
-            obs_x = SX.sym(f'obs_x_{i}')
-            obs_y = SX.sym(f'obs_y_{i}')
-            obstacles.append(ca.vertcat(obs_x, obs_y))
+        s_obstacles = []
+        d_obstacles = []
+        for i in range(self.max_samelaneobs_n):
+            s_obs_x = SX.sym(f's_obs_x_{i}')
+            s_obs_y = SX.sym(f's_obs_y_{i}')
+            s_obstacles.append(ca.vertcat(s_obs_x, s_obs_y))        
+        for i in range(self.max_difflaneobs_n):
+            d_obs_x = SX.sym(f'd_obs_x_{i}')
+            d_obs_y = SX.sym(f'd_obs_y_{i}')
+            d_obstacles.append(ca.vertcat(d_obs_x, d_obs_y))
     
-        ocp.model.p = ca.vertcat(ref, *obstacles)
+        ocp.model.p = ca.vertcat(ref, *s_obstacles, *d_obstacles)
         np = ocp.model.p.size1()
         ocp.parameter_values = zeros(np)
 
@@ -78,8 +89,8 @@ class MPC:
         # Costs 
         ocp.cost.cost_type   = "EXTERNAL"
         ocp.cost.cost_type_e = "EXTERNAL" 
-        Q = diag([50, 50, 1, 1])
-        R = diag([20, 10])
+        Q = diag([5e-1, 10, 1, 1e-3])
+        R = diag([6, 25])
 
         ocp.model.cost_expr_ext_cost = \
             Q[0,0]*(xref-x)**2 + \
@@ -100,7 +111,7 @@ class MPC:
 
         # Constraints 
         ocp.constraints.lbx =   array([ 0])
-        ocp.constraints.ubx =   array([40])
+        ocp.constraints.ubx =   array([self.max_vel])
         ocp.constraints.idxbx = array([2])
 
         ocp.constraints.lbu = array([-self.max_a, -self.max_steering_angle])
@@ -108,16 +119,20 @@ class MPC:
         ocp.constraints.idxbu = array([0, 1])
 
         # Nonlinear constraints -> Need to be changed for surrounding vehicles
-        obstacle_dist = []
-        XY = ca.vertcat(x, y)
-        for obs in obstacles:
-            # obs_XY = obs[:2]
-            # obstacle_dist.append(ca.dot(XY-obs_XY, XY-obs_XY))
-            obstacle_dist.append(fabs(x-obs[0]))
-        ocp.model.con_h_expr = ca.vertcat(*obstacle_dist)
+        s_obstacle_dist = []
+        d_obstacle_dist = []
+        for obs in s_obstacles:
+            s_obstacle_dist.append(fabs(x-obs[0]))
+        for obs in d_obstacles:
+            dx = x - obs[0]
+            dy = y - obs[1]
+            perp_dist = fabs(dx * ca.sin(psi) - dy * ca.cos(psi))
+            d_obstacle_dist.append(perp_dist)
+        
+        ocp.model.con_h_expr = ca.vertcat(*s_obstacle_dist, * d_obstacle_dist)
 
-        lh_obstacles = array([self.Length+self.d_safe] * self.max_obs_n)
-        uh_obstacles = array([1e+6] * self.max_obs_n)
+        lh_obstacles = array([self.Length+self.ds_safe] * self.max_samelaneobs_n + [self.Width+self.dn_safe] * self.max_difflaneobs_n)
+        uh_obstacles = array([1e+6] * self.max_samelaneobs_n + [1e+6] * self.max_difflaneobs_n)
         ocp.constraints.lh = lh_obstacles 
         ocp.constraints.uh = uh_obstacles
         nh = ocp.model.con_h_expr.size1()
@@ -126,7 +141,7 @@ class MPC:
         ocp.constraints.ush = zeros(nh)
         ocp.constraints.idxsh = array(range(nh))
 
-        ocp.cost.zl = ocp.cost.zu = ocp.cost.Zl = ocp.cost.Zu = 1e5 * ones(nh)
+        ocp.cost.zl = ocp.cost.zu = ocp.cost.Zl = ocp.cost.Zu = 1e2 * ones(nh)
 
         ocp.solver_options.tf = self.N * self.dt
         ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
@@ -144,7 +159,7 @@ class MPC:
 
         return ocp
 
-    def solve(self, x0, ref_traj, obs_traj):
+    def solve(self, x0, ref_traj, s_obs_traj, d_obs_traj):
         self.solver.set(0, "lbx", x0)
         self.solver.set(0, "ubx", x0)
         # if first solve
@@ -152,15 +167,14 @@ class MPC:
             for k in range(self.N):
                 self.solver.set(k,'x',x0)
 
-        # pad obs_traj
-        if obs_traj is not None:
-            padded_obs = zeros((self.max_obs_n, self.N, self.state_n))
-            padded_obs[:obs_traj.shape[0],:,:] = obs_traj
-            for k in range(self.N+1):
-                self.solver.set(k, "p", hstack([*ref_traj[k],*padded_obs[:,k-1,:2]]))
-        else:
-            for k in range(self.N+1):
-                self.solver.set(k, "p", hstack([*ref_traj[k]]))
+        s_padded_obs = zeros((self.max_samelaneobs_n, self.N, self.state_n))
+        s_padded_obs[:s_obs_traj.shape[0],:,:] = s_obs_traj
+
+        d_padded_obs = zeros((self.max_difflaneobs_n, self.N, self.state_n))
+        d_padded_obs[:d_obs_traj.shape[0],:,:] = d_obs_traj
+
+        for k in range(self.N+1):
+            self.solver.set(k, "p", hstack([*ref_traj[k],*s_padded_obs[:,k-1,:2],*d_padded_obs[:,k-1,:2]]))
 
         status = self.solver.solve()
         if status != 0:
@@ -177,7 +191,7 @@ class MPC:
     def run(self, obs):
         ego_x = obs[0]['x']
         ego_y = obs[0]['y']
-        
+
         lane = self.vehicle.road.network.get_lane(self.ref_lane)
         local_s = lane.local_coordinates([ego_x, ego_y])[0]
 
@@ -192,27 +206,38 @@ class MPC:
             future_ref[i, 1] = y_ref
             future_ref[i, 2] = self.ref_speed
             future_ref[i, 3] = heading_i
-        
-        # TEST: 같은 레인에 있는 차량만 obstacle로 사용
-        nearby_obs = []
-        for o in obs[1:]:
-            if o['lane_index'] == 2:
-                dx = o['x'] - ego_x
-                dy = o['y'] - ego_y
-                dist = hypot(dx, dy)
-                if dist <= 50.0:
-                    nearby_obs.append(o)
 
-        nearby_obs = nearby_obs[:self.max_obs_n]
-        obs_trajectories = zeros((len(nearby_obs), self.N, self.state_n))
+        # TEST: WITH Surrounding vehicles
+        samelane_obs = []
+        difflane_obs = []
+        for o in obs[1:]:
+            dx = o['x'] - ego_x
+            dy = o['y'] - ego_y
+            dist = hypot(dx, dy)
+            if dist <= 50.0:
+                if o['lane_index'] == self.vehicle.lane_index[-1]:
+                    samelane_obs.append(o)
+                else:
+                    difflane_obs.append(o)
+        samelane_obs = samelane_obs[:self.max_samelaneobs_n]
+        difflane_obs = difflane_obs[:self.max_difflaneobs_n]
+
+        s_obs_trajectories = zeros((len(samelane_obs), self.N, self.state_n))
+        d_obs_trajectories = zeros((len(difflane_obs), self.N, self.state_n))
 
         # 예측 trajectory 생성
-        for obs_id, o in enumerate(nearby_obs):
+        for obs_id, o in enumerate(samelane_obs):
             for stepN in range(self.N):
-                obs_trajectories[obs_id, stepN, 0] = o['x'] + o['vx'] * stepN * self.dt
-                obs_trajectories[obs_id, stepN, 1] = o['y'] + o['vy'] * stepN * self.dt
-                obs_trajectories[obs_id, stepN, 2] = o['vx']
-                obs_trajectories[obs_id, stepN, 3] = o['heading']
+                s_obs_trajectories[obs_id, stepN, 0] = o['x'] + o['vx'] * stepN * self.dt
+                s_obs_trajectories[obs_id, stepN, 1] = o['y'] + o['vy'] * stepN * self.dt
+                s_obs_trajectories[obs_id, stepN, 2] = o['vx']
+                s_obs_trajectories[obs_id, stepN, 3] = o['heading']
+        for obs_id, o in enumerate(difflane_obs):
+            for stepN in range(self.N):
+                d_obs_trajectories[obs_id, stepN, 0] = o['x'] + o['vx'] * stepN * self.dt
+                d_obs_trajectories[obs_id, stepN, 1] = o['y'] + o['vy'] * stepN * self.dt
+                d_obs_trajectories[obs_id, stepN, 2] = o['vx']
+                d_obs_trajectories[obs_id, stepN, 3] = o['heading']
 
         x0 = array([
             obs[0]['x'],
@@ -221,7 +246,7 @@ class MPC:
             obs[0]['heading']
         ])
 
-        result = self.solve(x0, future_ref, obs_traj=obs_trajectories)
+        result = self.solve(x0, future_ref, s_obs_trajectories, d_obs_trajectories)
 
         a = result[0]
         delta = result[1]
